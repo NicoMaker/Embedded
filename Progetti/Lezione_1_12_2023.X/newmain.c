d73b781303a35f571e8282395fedaf0f2f9bca2e
Nico Maker <nicola.marano02@gmail.com>
2023-12-01 10:52:54 +0100

    Lezione 7

---
+++ Progetti/Lezione_1_12_2023.X/newmain.c
@@ -0,0 +1,45 @@
+/*
+ * File:   newmain.c
+ * Author: nicol
+ *
+ * Created on 1 dicembre 2023, 9.16
+ */
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config WRT = OFF 
+#pragma config CP = OFF  
+
+#define _XTAL_FREQ 8000000
+#include <xc.h>
+
+void initUART(long int);
+
+void main(void) {
+    
+    initUART(9600);
+    
+    while(1)
+    {
+        TXREG = 'c'; // in automatico trasmette 
+        __delay_ms(1000);
+    } 
+    
+    return;
+}
+
+void initUART(long int baudRate)
+{
+   TRISCbits.TRISC7 = 1; // impongo input RX
+   TRISCbits.TRISC6 = 0;  // impongo output TX
+   
+   TXSTAbits.TXEN = 1; //abilito sottomodulo di TX
+   RCSTAbits.SPEN = 1; // abilito il modulo UART
+   RCSTA |= 0x90;
+   SPBRG = (_XTAL_FREQ/(long)(64UL*baudRate))-1; // uso variabile del calcolo senza segno UL
+   
+}
