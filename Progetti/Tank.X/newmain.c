f2d5dc1dbf6485a835f648ca8eaa4013d9b8f743
Nico Maker <nicola.marano02@gmail.com>
2023-12-22 12:54:39 +0100

    Prime Versione tank

---
+++ Progetti/Tank.X/newmain.c
@@ -0,0 +1,16 @@
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = ON
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
+
+
+#include <xc.h>
+#define _XTAL_FREQ 8000000
+
+void main(void) { 
+    
+}
