21bea99a9ae8d2ccd8f65ee5bf2a28cab6097d84
Nico Maker <nicola.marano02@gmail.com>
2024-03-02 14:40:16 +0100

    Argomento 3 parte stage e inizo versione 3 stage

---
+++ Lavoro Durante Stage/3 parte/Progetto_3_Parte.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: nicol
+ *
+ * Created on 2 marzo 2024, 14.38
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ Lavoro Durante Stage/3 parte/Progetto_3_Parte.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Progetto_3_Parte.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Progetto_3_Parte.X.production.hex
