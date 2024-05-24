fd3f30f7cc5b8621b0d6dcad1c226d9b31dd649f
Nicola <nicola.marano02@gmail.com>
2024-05-24 11:41:08 +0200

    codice modificato

--- Lezioni/10 lezione/main.c
+++ Lezioni/10 lezione/main.c
@@ -119,6 +119,40 @@ void main(void) {
                 break;
         }
     }
+    // imposto i tris
+    TRISB = 0x00;
+    TRISD = 0x00;
+    // TRISA = 0x01;
+    TRISA = 0xFF;
+    TRISC = 0x00;
+
+    // imposto adcon
+    // ADCON0 = 0x01;
+    // ADCON0 = 0x09;
+    ADCON0 = 0x11;
+    // ADCON1 = 0x8E
+    ADCON1 = 0x80;
+    PORTCbits.RC5 = 1;
+    __delay_us(20);
+
+    while (1) {
+        // ADCON0 |= 0x04;
+        ADCON0bits.GO = 1;
+
+        while (ADCON0bits.GO != 0);
+
+        PORTB = ADRESL;
+        PORTD = ADRESH;
+
+        int temperature = ADRESL + (ADRESH << 8);
+
+        if (temperature > 81)
+            PORTCbits.RC2 = 1;
+        else if (temperature < 79)
+            PORTCbits.RC2 = 0;
+
+        __delay_ms(20);
+    }
 }
 
 void initLCD() {
